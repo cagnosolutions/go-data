2657b4c0c3bc70bddb2acbda269c9050821bb33c
Scott Cagno <scottiecagno@gmail.com>
2022-05-13 19:50:11 -0400

    [sac] -- working on the disk manager and page

--- pkg/bits/modbits.go
+++ pkg/bits/modbits.go
@@ -8,7 +8,7 @@ func SwapByteOrder(n uint32) uint32 {
 	return b0<<24 | b1<<16 | b2<<8 | b3<<0
 }
 
-func ModBitLo(n uint32, pos uint32, on bool) uint32 {
+func ModBit32Lo(n uint32, pos uint32, on bool) uint32 {
 	var b uint32
 	if on {
 		b = 1
@@ -17,11 +17,29 @@ func ModBitLo(n uint32, pos uint32, on bool) uint32 {
 	return (n & ^mask) | (b << pos)
 }
 
-func ModBitHi(n uint32, pos uint32, on bool) uint32 {
+func ModBit32Hi(n uint32, pos uint32, on bool) uint32 {
 	var b uint32
 	if on {
 		b = 1
 	}
 	mask := 1<<31 - pos
+	return (n & ^mask) | (b<<31 - pos)
+}
+
+func ModBit16Lo(n uint16, pos uint16, on bool) uint16 {
+	var b uint16
+	if on {
+		b = 1
+	}
+	mask := uint16(1 << pos)
+	return (n & ^mask) | (b << pos)
+}
+
+func ModBit16Hi(n uint16, pos uint16, on bool) uint16 {
+	var b uint16
+	if on {
+		b = 1
+	}
+	mask := 1<<15 - pos
 	return (n & ^mask) | (b<<31 - pos)
 }
--- pkg/pager/page.go
+++ pkg/pager/page.go
@@ -1,20 +1,22 @@
 package pager
 
 const (
+	flagA = uint16(0x0001)
+	flagB = uint16(0x0002)
+	flagC = uint16(0x0004)
+	flagD = uint16(0x0008)
+
 	statusUsed = 0xffff
 	statusFree = 0x0000
 )
 
 type header struct {
-	pid     pageID
-	prevPid pageID
-	nextPid pageID
-	kind    uint16 // type of page
-	status  uint16 // status of page
-	free    uint16 // total free bytes in page
-	slots   uint16 // number of slots in page
-	freeLo  uint16 // free space lower offset
-	freeHi  uint16 // free space upper offset
+	pid   pageID // page id
+	kind  uint16 // kind & status of page
+	slots uint16 // number of slots in page
+	lower uint16 // free space lower offset
+	upper uint16 // free space upper offset
+	data  []byte // raw page data
 }
 
 type page struct {
@@ -25,48 +27,16 @@ type page struct {
 func newPage(pid pageID) *page {
 	return &page{
 		header: header{
-			pid:     pid,
-			prevPid: 0,
-			nextPid: 0,
-			kind:    0,
-			status:  statusUsed,
-			free:    pageSize - hdrSize,
-			slots:   0,
-			freeLo:  hdrSize,
-			freeHi:  pageSize,
+			pid:   pid,
+			kind:  0,
+			slots: 0,
+			lower: 0,
+			upper: 0,
 		},
 		data: make([]byte, pageSize),
 	}
 }
 
-func (p *page) init() {
-
-}
-
 func (p *page) getPageID() pageID {
 	return p.pid
-}
-
-func (p *page) aaa() {
-
-}
-
-func (p *page) bbb() {
-
-}
-
-func (p *page) ccc() {
-
-}
-
-func (p *page) ddd() {
-
-}
-
-func (p *page) eee() {
-
-}
-
-func (p *page) fff() {
-
 }
