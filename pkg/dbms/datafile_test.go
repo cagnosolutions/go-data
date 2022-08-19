dce17f14fd83452d32f1ae29920de155b72a62c5
Scott Cagno <scottiecagno@gmail.com>
2022-08-19 19:34:00 -0400

    [sac] -- working on some stuff for the dbms  **REFACTORING

--- pkg/dbms/datafile_test.go
+++ pkg/dbms/datafile_test.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -36,17 +37,26 @@ func makeDataFileName(id int) string {
 	return fmt.Sprintf("%s%.8x%s", filePrefix, id, fileSuffix)
 }
 
-// Namespace is a namespace struct
-type Namespace struct {
-	meta  *os.File
-	data  *os.File
-	dir   string
-	files []string
+func getDataFileID(name string) uint32 {
+	id, err := strconv.ParseUint(name, 16, 32)
+	if err != nil {
+		panic(err)
+	}
+	return uint32(id)
 }
 
-func OpenNamespace(base, namespace string) (*Namespace, error) {
+// Namespace is a namespace struct
+type Namespace struct {
+	meta *os.File
+	data *os.File
+	base string
+	name string
+}
+
+// OpenNamespace opens and returns a new *Namespace
+func OpenNamespace(base, name string) (*Namespace, error) {
 	// Add namespace string to base to make complete dir
-	dir := filepath.Join(base, namespace)
+	dir := filepath.Join(base, name)
 	// Strip of any suffixes (if there are any) from the path
 	dir = strings.Replace(dir, filepath.Ext(dir), "", -1)
 	// Clean the path
@@ -56,40 +66,43 @@ func OpenNamespace(base, namespace string) (*Namespace, error) {
 	}
 	// Create a new namespace instance we can return later
 	ns := &Namespace{
-		meta:  nil,
-		data:  nil,
-		dir:   dir,
-		files: make([]string, 0),
+		meta: nil,
+		data: nil,
+		base: dir,
+		name: name,
 	}
 	// Set up our filenames and file pointers for later
 	metaName := filepath.Join(dir, metaFile)
-	dataName := filepath.Join(dir, newDataFile)
 	// Check to see if we need to create the files
-	_, err = os.Stat(dir)
+	_, err = os.Stat(metaName)
 	if os.IsNotExist(err) {
 		// Touch any directories and/or file
 		err = os.MkdirAll(dir, os.ModeDir|dataFilePerm)
 		if err != nil {
 			return nil, err
 		}
-		// Open our meta file, and our new data file
+		// Create our meta file
 		ns.meta, err = os.OpenFile(metaName, os.O_CREATE|os.O_RDWR|os.O_SYNC, dataFilePerm)
 		if err != nil {
 			return nil, err
 		}
-		ns.data, err = os.OpenFile(dataName, os.O_CREATE|os.O_RDWR|os.O_SYNC, dataFilePerm)
-		if err != nil {
-			return nil, err
-		}
-		// Add our files to our files set in the namespace
-		ns.files = append(ns.files, filepath.Base(metaName))
-		ns.files = append(ns.files, filepath.Base(dataName))
 		// Now, we can return without walking the directory (because this is the first time)
 		return ns, nil
 	}
-	// Otherwise, we have to walk the directory
-	err = filepath.WalkDir(
-		dir, func(lpath string, info fs.DirEntry, err error) error {
+	// Otherwise, we have to open our existing meta file
+	ns.meta, err = os.OpenFile(metaName, os.O_RDWR|os.O_SYNC, dataFilePerm)
+	if err != nil {
+		return nil, err
+	}
+	// And then, we can return
+	return ns, nil
+}
+
+// Walk walks a directory, running the supplied function for each glob match encountered
+func (ns *Namespace) Walk(matchGlob string, fn func(de fs.DirEntry) error) error {
+	// Walk our directory path
+	err := filepath.WalkDir(
+		filepath.Join(ns.base, ns.name), func(lpath string, info fs.DirEntry, err error) error {
 			// Handle any local path errors
 			if err != nil {
 				log.Printf("prevent panic by handling failure accessing: %q: %v\n", lpath, err)
@@ -98,23 +111,10 @@ func OpenNamespace(base, namespace string) (*Namespace, error) {
 			// Check for local file match
 			matched, err := filepath.Match(matchGlob, lpath)
 			if !info.IsDir() && matched {
-				// We have a match, append to our namespace file list
-				ns.files = append(ns.files, filepath.Base(lpath))
-				// Check to see if the file is the meta file
-				if lpath == metaName {
-					// It is the meta file, so we will open it up
-					ns.meta, err = os.OpenFile(metaName, os.O_CREATE|os.O_RDWR|os.O_SYNC, dataFilePerm)
-					if err != nil {
-						return err
-					}
-				}
-				// Check to see if the file is the current data file
-				if lpath == dataName {
-					// It is the current data file, so we will open it up
-					ns.data, err = os.OpenFile(dataName, os.O_CREATE|os.O_RDWR|os.O_SYNC, dataFilePerm)
-					if err != nil {
-						return err
-					}
+				// We have a match, run function...
+				err = fn(info)
+				if err != nil {
+					return err
 				}
 				// Otherwise, we will just keep going...
 			}
@@ -122,18 +122,23 @@ func OpenNamespace(base, namespace string) (*Namespace, error) {
 			return nil
 		},
 	)
-	return ns, nil
+	// Check to see if there were any errors to return
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
+// Close closes the namespace and files
 func (ns *Namespace) Close() error {
 	err := ns.meta.Close()
 	if err != nil {
 		return err
 	}
-	err = ns.data.Close()
-	if err != nil {
-		return err
-	}
+	// err = ns.data.Close()
+	// if err != nil {
+	// 	return err
+	// }
 	return nil
 }
 
